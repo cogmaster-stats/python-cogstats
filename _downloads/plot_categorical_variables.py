7289db91a67aeb1380add334a5b27505b0021b01
Gael Varoquaux <gael.varoquaux@normalesup.org>
2013-11-24 18:00:51 +0100

    Make install

---
+++ _downloads/plot_categorical_variables.py
@@ -0,0 +1,20 @@
+"""
+Plotting quantities from a CSV file
+=====================================
+
+This example loads from a CSV file data with mixed numerical and
+categorical entries, and plots a few quantities, separately for females
+and males, thanks to the pandas integrating plotting tool (that uses
+matplotlib behind the scene).
+
+"""
+
+import pandas
+
+import matplotlib.pyplot as plt
+
+data = pandas.read_csv('brain_size.csv', sep=';')
+gender_data = data.groupby('Gender')
+gender_data.boxplot(column=['FSIQ', 'VIQ', 'PIQ'])
+
+plt.show()
---
+++ _sources/fmri_signals.txt
@@ -0,0 +1,5 @@
+=================
+FMRI signals
+=================
+
+Here we will cover fMRI signals and their analysis.
