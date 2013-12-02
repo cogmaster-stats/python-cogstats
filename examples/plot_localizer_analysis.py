2daba36602e0d829e7c840d8552002ca8feb6a0b
Gael Varoquaux <gael.varoquaux@normalesup.org>
2013-12-02 22:19:32 +0100

    Rename and simplify

--- examples/plot_localizer_analysis.py
+++ examples/plot_localizer_analysis.py
@@ -15,10 +15,9 @@ More specifically:
 
 """
 
-from os import mkdir, getcwd, path
+from os import mkdir, path
 
 import numpy as np
-
 import matplotlib.pyplot as plt
 
 from nibabel import save
@@ -34,45 +33,31 @@ from nipy.labs.viz import plot_map, cm
 # Data and analysis parameters
 #######################################
 
-# volume mask
-# This dataset is large
-data_file = 's12069_swaloc1_corr.nii.gz'
-paradigm_file = 'localizer_paradigm.csv'
-
 # timing
 n_scans = 128
 tr = 2.4
-
 # paradigm
 frametimes = np.linspace(0.5 * tr, (n_scans - .5) * tr, n_scans)
 
-# confounds
-hrf_model = 'canonical with derivative'
-drift_model = "cosine"
-hfcut = 128
-
 # write directory
-write_dir = path.join(getcwd(), 'results')
+write_dir = 'results'
 if not path.exists(write_dir):
     mkdir(write_dir)
-
-print('Computation will be performed in directory: %s' % write_dir)
 
 ########################################
 # Design matrix
 ########################################
 
-print('Loading design matrix...')
+paradigm = load_paradigm_from_csv_file('localizer_paradigm.csv')['0']
 
-paradigm = load_paradigm_from_csv_file(paradigm_file)['0']
+design_matrix = make_dmtx(frametimes, paradigm,
+                          hrf_model='canonical with derivative',
+                          drift_model="cosine", hfcut=128)
 
-design_matrix = make_dmtx(frametimes, paradigm, hrf_model=hrf_model,
-                          drift_model=drift_model, hfcut=hfcut)
-
+# Plot the design matrix
 ax = design_matrix.show()
 ax.set_position([.05, .25, .9, .65])
 ax.set_title('Design matrix')
-
 plt.savefig(path.join(write_dir, 'design_matrix.png'))
 
 #########################################
@@ -102,22 +87,19 @@ contrasts["computation-sentences"] = contrasts["computation"] -  \
                                      contrasts["sentences"]
 contrasts["reading-visual"] = contrasts["sentences"] * 2 - \
                               contrasts["damier_H"] - contrasts["damier_V"]
-contrasts['effects_of_interest'] = np.eye(25)[:20:2]
 
 ########################################
 # Perform a GLM analysis
 ########################################
 
-print('Fitting a GLM (this takes time)...')
-fmri_glm = FMRILinearModel(data_file, design_matrix.matrix,
-                           mask='compute')
+fmri_glm = FMRILinearModel('s12069_swaloc1_corr.nii.gz',
+                           design_matrix.matrix, mask='compute')
 fmri_glm.fit(do_scaling=True, model='ar1')
 
 #########################################
 # Estimate the contrasts
 #########################################
 
-print('Computing contrasts...')
 for index, (contrast_id, contrast_val) in enumerate(contrasts.items()):
     print('  Contrast % 2i out of %i: %s' %
           (index + 1, len(contrasts), contrast_id))
@@ -134,6 +116,5 @@ for index, (contrast_id, contrast_val) in enumerate(contrasts.items()):
              title=contrast_id)
     plt.savefig(path.join(write_dir, '%s_z_map.png' % contrast_id))
 
-print("All the  results were witten in %s" % write_dir)
 
 plt.show()
